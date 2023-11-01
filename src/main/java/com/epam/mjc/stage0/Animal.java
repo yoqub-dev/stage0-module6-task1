ac4cfbc9472ec961586ae4054047554d89ac4cac
yoqub-dev <yoqub.epam@gmail.com>
2023-11-01 19:52:49 +0500

    task17

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
 
-public class Animal {
+import lombok.Data;
 
+@Data
+public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    private String getPawNoun() {
+        return numberOfPaws == 1 ? "paw" : "paws";
+    }
+
+    public String getDescription() {
+        String furStatus = hasFur ? "a" : "no";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + getPawNoun() + " and " + furStatus + " fur.";
+    }
 }
